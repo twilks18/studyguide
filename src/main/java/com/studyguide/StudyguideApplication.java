349fb027b56ab48dd380eef2957edddb8bc5e7e2
TaNeisha <twilks18@gmail.com>
2021-03-15 19:58:15 -0500

    First commit

---
+++ src/main/java/com/studyguide/StudyguideApplication.java
@@ -0,0 +1,13 @@
+package com.studyguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyguide/StudyguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
