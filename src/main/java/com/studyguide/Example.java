60b89d45f83a768a367b0b2350366c69c35c3699
TaNeisha <twilks18@gmail.com>
2021-03-17 18:24:34 -0500

    Updated example class

--- src/main/java/com/studyguide/Example.java
+++ src/main/java/com/studyguide/Example.java
@@ -5,6 +5,16 @@ public class Example {
     public static void main(String args[]) {
 //        Question(Long id, String question, String answer)
 
+
+        /**
+         * The class name is Example(which is a object)
+         * The new keyword is used to instantiate or create that object
+         * Whenever you create a new object it must also be given a name such as q1 and
+         * it must also be given a type and in this case it is a Question.
+         */
+
+        //-Here is another way to comment out code
+
         Question q1 = new Question(10, "What is your name", "Pamela" );
         Question q2 = new Question(11, "What month is your birthday?", "January");
         Question q3 = new Question();
