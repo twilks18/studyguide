417099d1707ca7ff29110fb46f7516e7f0ec1eb7
pwilks62 <pwilks8896@gmail.com>
2021-03-17 20:17:57 -0500

    adding chapter class

---
+++ src/main/java/com/studyguide/Chapter.java
@@ -0,0 +1,60 @@
+package com.studyguide;
+
+import java.util.List;
+
+public class Chapter {
+    /**
+     * chapter is the name of this class/object i
+     * private is a java key word and it is an access modifier
+     * access modifier dertermines what fields or methods that can be access by other classeses
+     * int is a java key word that idenifies a field or method type
+     * the id is the name of a field for this class
+     */
+    private int id;
+    private List<Question> questions;
+    private String name;
+
+    /*
+     *
+     * arg(arguements) constructor which means this contructor contains arguements
+     * this constructor is going to be use to construct our objects/ chapter
+     *
+     *
+     */
+    public Chapter(int id, List<Question> questions, String name) {
+        this.id = id;
+        this.questions = questions;
+        this.name = name;
+    }
+
+    /*
+    * no arg constructor which means this constrictpr dose not contains arguements
+    * this constructor is going to be used to comstruct our objects / objects
+     */
+    public Chapter() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/studyguide/Example.java
+++ src/main/java/com/studyguide/Example.java
@@ -1,5 +1,8 @@
 package com.studyguide;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Example {
 
     public static void main(String args[]) {
@@ -27,6 +30,18 @@ public class Example {
         System.out.println("Question 2 - " + q2.getQuestion() +": "+  q2.getAnswer());
         System.out.println("Question 3 - " + q3.getQuestion() +": "+  q3.getAnswer());
 
+        List<Question> questions = new ArrayList<>();
+
+        questions.add(q1);
+        questions.add(q2);
+        questions.add(q3);
+
+        Chapter c1= new Chapter(2,questions, "Ready");
+
+        System.out.println("chapter id:" + c1.getId());
+        System.out.println("chapter name: " + c1.getName());
+        System.out.println("chaptr questions: " +  c1.getQuestions().toString()git status
+        );
 
     }
 }
